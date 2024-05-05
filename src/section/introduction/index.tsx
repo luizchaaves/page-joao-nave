9c5864f013bc7258d51572951f4a2e37b3c07759
Luiz Otávio <luizotavio0106@outlook.com>
2024-05-05 11:45:19 -0300

    refactor: change page title

--- src/section/introduction/index.tsx
+++ src/section/introduction/index.tsx
@@ -21,7 +21,7 @@ const Introduction = ({ background }: Props) => {
             <Section background={background.image.src}>
                 <Wrapper>
                     <Content data-aos="fade-down">
-                        <h1>Desvendando os fornecedores <span>PREMIUM</span> chinês</h1>
+                        <h1>Desvendando os fornecedores <span>PREMIUM</span> chineses</h1>
                         <p>Tenha o contato de inúmeros fornecedores chineses e descubra como fiz para faturar acima de 7 dígitos por ano!</p>
                         <Button onClick={scrollToCoursesButton}>Saiba mais!</Button>
                     </Content>
