96a741d2383f17ab32b8b8e6697d76277db3d1d0
Luiz Otávio <luizotavio0106@outlook.com>
2024-05-13 23:47:43 -0300

    feat: export star icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,13 @@ import Button from './button';
 import CardCourses from './card-courses';
 import CardInformation from './card-information';
 import Divider from './divider';
+import StarIcon from './star-icon';
 
 export {
     Accordion,
     Button,
     CardCourses,
     CardInformation,
-    Divider
+    Divider,
+    StarIcon
 }
